f251b15d5831b2f5b007b0958b6ac52bb22186ad
Marvin Theiss <mrvnthss@mail.de>
2024-04-13 16:25:02 +0200

    Modify algorithm to compute incidence matrices for FPPs

--- dobble/utils.py
+++ dobble/utils.py
@@ -6,6 +6,8 @@ finite projective planes.
 Functions:
     - _is_prime: Check if a number is prime.
     - _is_prime_power: Check if a number is a prime power.
+    - _get_permutation_matrix: Return the permutation matrix
+        corresponding to the permutation.
     - compute_incidence_matrix: Compute the incidence matrix of a finite
         projective plane.
 """
@@ -60,6 +62,35 @@ def _is_prime_power(num: float) -> bool:
     return False
 
 
+def _get_permutation_matrix(permutation: np.ndarray) -> np.ndarray:
+    """Return the permutation matrix corresponding to the permutation.
+
+    Args:
+        permutation (np.ndarray): The permutation to be converted to a
+            permutation matrix.
+
+    Returns:
+        np.ndarray: The permutation matrix corresponding to the
+            permutation.
+
+    Raises:
+        ValueError: If the passed argument is not a valid permutation.
+    """
+    # Check if the permutation is valid
+    if (not isinstance(permutation, np.ndarray)
+            or permutation.ndim != 1
+            or set(permutation) != set(range(1, len(permutation) + 1))
+            or len(permutation) == 0):
+        raise ValueError("Invalid permutation.")
+
+    # Construct permutation matrix
+    size = len(permutation)
+    permutation_matrix = np.zeros((size, size), dtype=np.uint8)
+    permutation_matrix[np.arange(size), permutation - 1] = 1
+
+    return permutation_matrix
+
+
 def compute_incidence_matrix(order: int) -> np.ndarray:
     """Compute the incidence matrix of a finite projective plane with
        specified order.
@@ -72,7 +103,7 @@ def compute_incidence_matrix(order: int) -> np.ndarray:
             lines and columns correspond to points.
 
     Raises:
-        ValueError: If the argument order is not a prime power.
+        ValueError: If the argument order is not a prime.
 
     Example:
         >>> compute_incidence_matrix(2)
@@ -84,37 +115,50 @@ def compute_incidence_matrix(order: int) -> np.ndarray:
                [0, 0, 1, 1, 0, 0, 1],
                [0, 0, 1, 0, 1, 1, 0]], dtype=uint8)
     """
-    if not _is_prime_power(order):
-        raise ValueError("The argument 'order' must be a prime power.")
+    if not _is_prime(order):
+        raise ValueError("The argument 'order' must be a prime.")
 
     # Number of points/lines of a finite projective plane of order n is given by n^2 + n + 1
     size = order ** 2 + order + 1
 
-    # Preallocate incidence matrix, where rows correspond to lines and columns correspond to points
+    # Preallocate incidence matrix
+    #   - rows correspond to lines
+    #   - columns correspond to points
     incidence_matrix = np.zeros((size, size), dtype=np.uint8)
 
-    # Determine which points are on the first line
-    which_line = 0
-    which_pts = list(range(order + 1))
-    incidence_matrix[which_line, which_pts] = 1
+    # Place 1s in upper left block
+    incidence_matrix[0, : order + 1] = 1
+    incidence_matrix[: order + 1, 0] = 1
 
-    # Determine which points are on the next n lines
-    for line in range(order):
-        which_line += 1
-        # The first n + 1 lines will all share point "0"
-        which_pts = [0]
-        start = (line + 1) * order + 1
-        end = start + order
-        which_pts.extend(list(range(start, end)))
-        incidence_matrix[which_line, which_pts] = 1
+    # Place 1s in remaining boundary blocks (i.e., top row & leftmost column of blocks)
+    start = order + 1
+    for block in range(1, order + 1):
+        stop = start + order
+        # top row of blocks
+        incidence_matrix[block, start:stop] = 1
+        # leftmost column of blocks
+        incidence_matrix[start:stop, block] = 1
+        start = stop
 
-    # Determine which points are on the final n^2 lines
-    for block in range(order):
-        for line in range(order):
-            which_line += 1
-            which_pts = [block + 1]
-            for pt in range(order):
-                which_pts.append(order * (pt + 1) + ((block * pt + line) % order) + 1)
-            incidence_matrix[which_line, which_pts] = 1
+    # Place 1s in remaining n x n block matrix
+    # NOTE: Each block is an n x n permutation matrix
+    for row, col in np.ndindex(order, order):
+        # Determine permutation matrix
+        if row == 0 or col == 0:
+            permutation_matrix = np.eye(order, dtype=np.uint8)
+        else:
+            leading_entry = (1 + row * col) % order
+            if leading_entry == 0:
+                leading_entry = order
+            permutation = (np.array(range(0, order)) + leading_entry) % order
+            permutation[permutation == 0] = order
+            permutation_matrix = _get_permutation_matrix(permutation)
+
+        # Place permutation matrix in incidence matrix
+        start_row = order + 1 + row * order
+        start_col = order + 1 + col * order
+        end_row = start_row + order
+        end_col = start_col + order
+        incidence_matrix[start_row:end_row, start_col:end_col] = permutation_matrix
 
     return incidence_matrix
--- tests/test_utils.py
+++ tests/test_utils.py
@@ -17,15 +17,22 @@ from dobble import utils
 
 # Constants for Testing
 NEGATIVE_NUMBER = -7
-PRIME_NUMBER = 13
-NON_PRIME_NUMBER = 14
+
+NON_PRIME_NUMBER = 2 * 7
+LARGE_NON_PRIME_NUMBER = 10 * 792
+
+SMALL_PRIME_NUMBER = 2
+PRIME_NUMBER = 241
 LARGE_PRIME_NUMBER = 7919
-LARGE_NON_PRIME_NUMBER = 7920
-PRIME_POWER = 8
-NON_PRIME_POWER = 10
+
+NON_PRIME_POWER = 2 * 5
+LARGE_NON_PRIME_POWER = (2 * 5) ** 4
+
+PRIME_POWER = 2 ** 3
 LARGE_PRIME_POWER = 3 ** 5
-LARGE_NON_PRIME_POWER = 10 ** 4
-SMALL_PRIME_POWER = 2
+
+INVALID_PERMUTATION = np.array([2, 1, 4])
+VALID_PERMUTATION = np.array([2, 4, 1, 3])
 
 
 def test_is_prime_with_zero():
@@ -40,20 +47,20 @@ def test_is_prime_with_negative_number():
     assert not utils._is_prime(NEGATIVE_NUMBER)
 
 
-def test_is_prime_with_prime_number():
-    assert utils._is_prime(PRIME_NUMBER)
-
-
 def test_is_prime_with_non_prime_number():
     assert not utils._is_prime(NON_PRIME_NUMBER)
 
 
-def test_is_prime_with_large_prime_number():
-    assert utils._is_prime(LARGE_PRIME_NUMBER)
-
-
 def test_is_prime_with_large_non_prime_number():
     assert not utils._is_prime(LARGE_NON_PRIME_NUMBER)
+
+
+def test_is_prime_with_prime_number():
+    assert utils._is_prime(PRIME_NUMBER)
+
+
+def test_is_prime_with_large_prime_number():
+    assert utils._is_prime(LARGE_PRIME_NUMBER)
 
 
 def test_is_prime_power_with_zero():
@@ -68,20 +75,40 @@ def test_is_prime_power_with_negative_number():
     assert not utils._is_prime_power(NEGATIVE_NUMBER)
 
 
-def test_is_prime_power_with_prime_power():
-    assert utils._is_prime_power(PRIME_POWER)
-
-
 def test_is_prime_power_with_non_prime_power():
     assert not utils._is_prime_power(NON_PRIME_POWER)
+
+
+def test_is_prime_power_with_large_non_prime_power():
+    assert not utils._is_prime_power(LARGE_NON_PRIME_POWER)
+
+
+def test_is_prime_power_with_prime_power():
+    assert utils._is_prime_power(PRIME_POWER)
 
 
 def test_is_prime_power_with_large_prime_power():
     assert utils._is_prime_power(LARGE_PRIME_POWER)
 
 
-def test_is_prime_power_with_large_non_prime_power():
-    assert not utils._is_prime_power(LARGE_NON_PRIME_POWER)
+def test_get_permutation_matrix_with_empty_permutation():
+    with pytest.raises(ValueError):
+        utils._get_permutation_matrix(np.array([], dtype=np.uint8))
+
+
+def test_get_permutation_matrix_with_invalid_permutation():
+    with pytest.raises(ValueError):
+        utils._get_permutation_matrix(INVALID_PERMUTATION)
+
+
+def test_get_permutation_matrix_with_valid_permutation():
+    expected_matrix = np.array([[0, 1, 0, 0],
+                                [0, 0, 0, 1],
+                                [1, 0, 0, 0],
+                                [0, 0, 1, 0]], dtype=np.uint8)
+    np.testing.assert_array_equal(
+        utils._get_permutation_matrix(VALID_PERMUTATION), expected_matrix
+    )
 
 
 def test_compute_incidence_matrix_with_zero():
@@ -99,7 +126,12 @@ def test_compute_incidence_matrix_with_negative_number():
         utils.compute_incidence_matrix(NEGATIVE_NUMBER)
 
 
-def test_compute_incidence_matrix_with_small_prime_power():
+def test_compute_incidence_matrix_with_non_prime_number():
+    with pytest.raises(ValueError):
+        utils.compute_incidence_matrix(NON_PRIME_NUMBER)
+
+
+def test_compute_incidence_matrix_with_small_prime_number():
     expected_matrix = np.array([[1, 1, 1, 0, 0, 0, 0],
                                 [1, 0, 0, 1, 1, 0, 0],
                                 [1, 0, 0, 0, 0, 1, 1],
@@ -109,15 +141,5 @@ def test_compute_incidence_matrix_with_small_prime_power():
                                 [0, 0, 1, 0, 1, 1, 0]], dtype=np.uint8)
 
     np.testing.assert_array_equal(
-        utils.compute_incidence_matrix(SMALL_PRIME_POWER), expected_matrix
+        utils.compute_incidence_matrix(SMALL_PRIME_NUMBER), expected_matrix
     )
-
-
-def test_compute_incidence_matrix_with_large_prime_power():
-    # Performance test
-    utils.compute_incidence_matrix(LARGE_PRIME_POWER)
-
-
-def test_compute_incidence_matrix_with_non_prime_power():
-    with pytest.raises(ValueError):
-        utils.compute_incidence_matrix(NON_PRIME_POWER)
