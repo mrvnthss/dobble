7b9e490e9bfb92938c5857b48658b80398b5226f
Marvin Theiss <mrvnthss@mail.de>
2024-05-23 17:06:52 +0200

    Restructure JSON file from OpenMoji

--- setup_openmoji.py
+++ setup_openmoji.py
@@ -16,13 +16,15 @@ import requests
 from dobble import constants
 
 
-# Directory containing OpenMoji data
-OPENMOJI_DIR = Path("data/openmoji")
-
 # Constants controlling which tasks to perform
-DOWNLOAD_EXTRACT_AND_ORGANIZE = True
-FIND_DUPLICATES = True
+DOWNLOAD_EXTRACT_AND_ORGANIZE = False
+FIND_DUPLICATES = False
 REMOVE_DUPLICATES = True
+RESTRUCTURE_JSON_FILE = True
+
+# OpenMoji data
+OPENMOJI_DIR = Path("data/openmoji")
+OPENMOJI_JSON = OPENMOJI_DIR / "openmoji.json"
 
 # OpenMoji emojis whose "annotations" parameter is not unique
 DUPLICATES = [
@@ -84,8 +86,7 @@ def organize_openmoji_data() -> None:
     OpenMoji.
     """
     # Read JSON file provided by OpenMoji
-    json_fpath = OPENMOJI_DIR / "openmoji.json"
-    with json_fpath.open("r", encoding="utf-8") as json_file:
+    with OPENMOJI_JSON.open("r", encoding="utf-8") as json_file:
         openmoji_data = json.load(json_file)
 
     # Create subdirectory for each unique "group" value
@@ -106,17 +107,14 @@ def organize_openmoji_data() -> None:
                 shutil.move(str(emoji_fpath), str(target_dir))
 
 
-def find_duplicates() -> None:
+def find_duplicates() -> dict[str, list[dict[str, str]]]:
     """Find emojis with duplicate annotations.
 
     This function finds emojis from the OpenMoji dataset that have a
-    non-unique "annotation" in the JSON file provided by OpenMoji.  It
-    then prints these duplicate annotations along with the hexcodes and
-    groups of the corresponding emojis.
+    non-unique "annotation" in the JSON file provided by OpenMoji.
     """
     # Read JSON file provided by OpenMoji
-    json_fpath = OPENMOJI_DIR / "openmoji.json"
-    with json_fpath.open("r", encoding="utf-8") as json_file:
+    with OPENMOJI_JSON.open("r", encoding="utf-8") as json_file:
         openmoji_data = json.load(json_file)
 
     # Count the number of appearances of each annotation
@@ -134,11 +132,22 @@ def find_duplicates() -> None:
             }
             duplicates[entry["annotation"]].append(emoji_data)
 
-    # Print emojis with duplicate annotations
+    return duplicates
+
+
+def print_duplicates(duplicates: dict[str, list[dict[str, str]]]) -> None:
+    """Print emojis with duplicate annotations.
+
+    This function prints emojis from the OpenMoji dataset that have a
+    non-unique "annotation" in the JSON file provided by OpenMoji.  It
+    prints the annotations along with the hexcodes and groups of all
+    emojis with that annotation.
+    """
     for annotation, emoji_data in duplicates.items():
-        print(annotation)
+        print(f"Annotation: {annotation}")
         for entry in emoji_data:
-            print(f'  ("{entry["hexcode"]}", "{entry["group"]}")')
+            print(f'\t("{entry["hexcode"]}", "{entry["group"]}")')
+        print()
 
 
 def remove_duplicates() -> None:
@@ -159,8 +168,7 @@ def remove_duplicates() -> None:
         color_fpath.unlink(missing_ok=True)
 
     # Read JSON file provided by OpenMoji
-    json_fpath = OPENMOJI_DIR / "openmoji.json"
-    with json_fpath.open("r", encoding="utf-8") as json_file:
+    with OPENMOJI_JSON.open("r", encoding="utf-8") as json_file:
         openmoji_data = json.load(json_file)
 
     # Remove entries corresponding to duplicates
@@ -169,8 +177,35 @@ def remove_duplicates() -> None:
     ]
 
     # Write updated data back to JSON file
-    with json_fpath.open("w", encoding="utf-8") as json_file:
+    with OPENMOJI_JSON.open("w", encoding="utf-8") as json_file:
         json.dump(openmoji_data, json_file, indent=2, ensure_ascii=False)
+
+
+def restructure_json_file() -> None:
+    """Restructure OpenMoji JSON file.
+
+    This function restructures the JSON file provided by OpenMoji in
+    such a way that the JSON file contains a dictionary with the emoji
+    "annotation" as keys and dictionaries containing the "group" and
+    "hexcode" parameters as values.
+    """
+    # Read JSON file provided by OpenMoji
+    with OPENMOJI_JSON.open("r", encoding="utf-8") as json_file:
+        openmoji_data = json.load(json_file)
+
+    # Create dictionary with desired format
+    restructured_data = {}
+    for entry in openmoji_data:
+        key = entry["annotation"]
+        restructured_data[key] = {
+            "group": entry["group"],
+            "hexcode": entry["hexcode"]
+        }
+
+    # Write restructured data to JSON file
+    restructured_json_fpath = OPENMOJI_DIR / "openmoji_restructured.json"
+    with restructured_json_fpath.open("w", encoding="utf-8") as json_file:
+        json.dump(restructured_data, json_file, indent=4)
 
 
 if __name__ == "__main__":
@@ -179,7 +214,11 @@ if __name__ == "__main__":
         organize_openmoji_data()
 
     if FIND_DUPLICATES:
-        find_duplicates()
+        duplicate_annotations = find_duplicates()
+        print_duplicates(duplicate_annotations)
 
     if REMOVE_DUPLICATES:
         remove_duplicates()
+
+    if RESTRUCTURE_JSON_FILE:
+        restructure_json_file()
