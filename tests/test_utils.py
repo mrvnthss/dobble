3b21b8230fcc5ace19f915a8b29d59094609ba41
Marvin Theiss <mrvnthss@mail.de>
2024-05-24 16:36:14 +0200

    Remove protected-access disable option from pylint in test_utils.py

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,4 +1,4 @@
-# pylint: disable=protected-access, missing-function-docstring, missing-module-docstring
+# pylint: disable=missing-function-docstring, missing-module-docstring
 
 from dobble import utils
 
