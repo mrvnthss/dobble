b354e6b2c234ab072b932334b93929f39bbac554
Marvin Theiss <mrvnthss@mail.de>
2024-05-25 16:40:28 +0200

    Implement rescale_img function

--- dobble/utils.py
+++ dobble/utils.py
@@ -4,10 +4,15 @@ Functions:
     is_integer: Check if a number is an integer.
     is_prime: Check if a number is prime.
     is_prime_power: Check if a number is a prime power.
+    rescale_img: Rescale a square image to fit content within inscribed
+      circle.
 """
 
 
 import math
+
+import numpy as np
+from PIL import Image
 
 
 def is_integer(num: int | float) -> bool:
@@ -66,3 +71,75 @@ def is_prime_power(num: int | float) -> bool:
                 state = True
                 break
     return state
+
+
+def rescale_img(
+        img: Image.Image,
+        padding: float = 0.1
+) -> Image.Image:
+    """Rescale a square image to fit content within inscribed circle.
+
+    This function expects as input a square image in RGBA mode, and
+    rescales the image such that all non-transparent content fits
+    within the inscribed circle.  The padding argument specifies the
+    fraction of the image size to be added as padding around the image
+    content.
+
+    Args:
+        img: The square image whose content is to be rescaled.
+        padding: The padding around the image content as a fraction of
+          the image size.  Must be in the range [0, 1).
+
+    Returns:
+        The image in RGBA mode with rescaled content, but identical
+        dimensions.
+
+    Raises:
+        ValueError: If the image is not a square image in RGBA mode or
+          if the padding is not in the range [0, 1).
+    """
+
+    # Check if image is in RGBA mode
+    if img.mode != "RGBA":
+        raise ValueError("Image must be in RGBA mode.")
+
+    # Convert image to NumPy array, then check if it's a square image
+    img_np = np.array(img)
+    if img_np.shape[0] != img_np.shape[1]:
+        raise ValueError("Image must be a square image.")
+
+    # Check if padding is within valid range
+    if not 0 <= padding < 1:
+        raise ValueError("Padding must be in the range [0, 1).")
+
+    # Determine non-transparent pixels, and return image if all pixels are transparent
+    non_transparent = np.argwhere(img_np[:, :, -1] > 0)
+    if non_transparent.shape[0] == 0:
+        return img
+
+    # Determine Euclidean distance (in pixels) of outermost non-transparent pixel
+    img_size = img_np.shape[0]
+    radius = img_size // 2
+    non_transparent = non_transparent - radius + 0.5  # shift center to (0, 0)
+    max_distance = np.max(np.linalg.norm(non_transparent, axis=1))
+
+    # Compute target size in pixels
+    rescaling_factor = (1 - padding) * radius / max_distance
+    target_size = int(img_size * rescaling_factor)
+
+    # Rescale image
+    if img_size == target_size:
+        return img
+    img = img.resize((target_size, target_size))
+
+    # Offset to center rescaled image on canvas / crop to fit original canvas
+    offset = abs(img_size - target_size) // 2
+
+    if target_size < img_size:  # paste rescaled smaller image onto canvas of original size
+        canvas = Image.new("RGBA", (img_size, img_size), (0, 0, 0, 0))
+        canvas.paste(img, (offset, offset), mask=img)
+        img = canvas
+    elif target_size > img_size:  # crop rescaled larger image to fit original canvas
+        img = img.crop((offset, offset, offset + img_size, offset + img_size))
+
+    return img
--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,5 +1,9 @@
 # pylint: disable=missing-function-docstring, missing-module-docstring
 
+import numpy as np
+from PIL import Image, ImageDraw
+import pytest
+
 from dobble import utils
 
 
@@ -20,6 +24,13 @@ LARGE_NON_PRIME_POWER = (2 * 5) ** 4
 
 PRIME_POWER = 2 ** 3
 LARGE_PRIME_POWER = 3 ** 5
+
+RGB_IMAGE = Image.new("RGB", (100, 100))
+NON_SQUARE_IMAGE = Image.new("RGBA", (100, 200))
+FULLY_TRANSPARENT_IMAGE = Image.new("RGBA", (100, 100), (0, 0, 0, 0))
+
+NEGATIVE_PADDING = -0.1
+INVALID_PADDING = 1
 
 
 def test_is_integer_with_integer():
@@ -88,3 +99,64 @@ def test_is_prime_power_with_prime_power():
 
 def test_is_prime_power_with_large_prime_power():
     assert utils.is_prime_power(LARGE_PRIME_POWER)
+
+
+def test_rescale_img_with_rgb_image():
+    with pytest.raises(ValueError):
+        utils.rescale_img(RGB_IMAGE)
+
+
+def test_rescale_img_with_non_square_image():
+    with pytest.raises(ValueError):
+        utils.rescale_img(NON_SQUARE_IMAGE)
+
+
+def test_rescale_img_with_negative_padding():
+    with pytest.raises(ValueError):
+        utils.rescale_img(RGB_IMAGE, padding=NEGATIVE_PADDING)
+
+
+def test_rescale_img_with_invalid_padding():
+    with pytest.raises(ValueError):
+        utils.rescale_img(RGB_IMAGE, padding=INVALID_PADDING)
+
+
+def test_rescale_img_with_fully_transparent_image():
+    np.testing.assert_array_equal(
+        np.array(utils.rescale_img(FULLY_TRANSPARENT_IMAGE)), np.array(FULLY_TRANSPARENT_IMAGE)
+    )
+
+
+def test_rescale_img_with_inscribed_circle():
+    # Create a transparent square image with white circle of maximum size
+    img = Image.new("RGBA", (100, 100), (0, 0, 0, 0))
+    draw = ImageDraw.Draw(img)
+    draw.ellipse([0, 0, 99, 99], fill="white")
+
+    np.testing.assert_array_equal(
+        np.array(utils.rescale_img(img, padding=0)), np.array(img)
+    )
+
+
+def test_rescale_img_with_large_square():
+    # Case: target_size < img_size
+    # Create a transparent square image with large white square in the center
+    img = Image.new("RGBA", (100, 100), (0, 0, 0, 0))
+    draw = ImageDraw.Draw(img)
+    draw.rectangle([10, 10, 90, 90], fill="white")
+
+    # Check that rescaled image contains fewer non-transparent pixels
+    rescaled_img = utils.rescale_img(img, padding=0)
+    assert np.sum(np.array(rescaled_img)[:, :, -1] > 0) < np.sum(np.array(img)[:, :, -1] > 0)
+
+
+def test_rescale_img_with_small_square():
+    # Case: target_size > img_size
+    # Create a transparent square image with small white square in the center
+    img = Image.new("RGBA", (100, 100), (0, 0, 0, 0))
+    draw = ImageDraw.Draw(img)
+    draw.rectangle([40, 40, 60, 60], fill="white")
+
+    # Check that rescaled image contains more non-transparent pixels
+    rescaled_img = utils.rescale_img(img, padding=0)
+    assert np.sum(np.array(rescaled_img)[:, :, -1] > 0) > np.sum(np.array(img)[:, :, -1] > 0)
